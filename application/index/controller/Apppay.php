9cdf4f41a0428bda7f7ec2ca49ae25ad257522fa
yangjq <yangjq_1991@163.com>
2018-10-23 21:55:41 +0800

    订单相关接口

---
+++ application/index/controller/Apppay.php
@@ -0,0 +1,245 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ls19980819
+ * Date: 2018/8/28
+ * Time: 9:35
+ */
+
+namespace app\index\controller;
+
+use think\Controller;
+use think\Db;
+use think\Loader;
+use think\Model;
+
+Loader::import('wxpay.WxPayApi', EXTEND_PATH);
+
+class Apppay extends Controller
+{
+    //传输给微信的参数组装成xml格式发送
+    public function ToXml($data = array())
+    {
+        if (!is_array($data) || count($data) <= 0) {
+            return '数组异常';
+        }
+
+        $xml = "<xml>";
+        foreach ($data as $key => $val) {
+            if (is_numeric($val)) {
+                $xml .= "<" . $key . ">" . $val . "</" . $key . ">";
+            } else {
+                $xml .= "<" . $key . "><![CDATA[" . $val . "]]></" . $key . ">";
+            }
+        }
+        $xml .= "</xml>";
+        return $xml;
+    }
+
+    //随机字符串
+    function rand_code()
+    {
+        $str = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';//62个字符
+        $str = str_shuffle($str);
+        $str = substr($str, 0, 32);
+        return $str;
+    }
+
+    //获取签名
+    private function getSign($params)
+    {
+        $WxPayConfig = new \WxPayConfig();
+        ksort($params);        //将参数数组按照参数名ASCII码从小到大排序
+        foreach ($params as $key => $item) {
+            if (!empty($item)) {         //剔除参数值为空的参数
+                $newArr[] = $key . '=' . $item;     // 整合新的参数数组
+            };
+        }
+        $stringA = implode("&", $newArr);         //使用 & 符号连接参数
+//        $stringSignTemp = $stringA . "&key=" . $WxPayConfig::KEY;        //拼接key
+        $stringSignTemp = $stringA . "&key=yi8d938uuuuud23432432xxxcg444444";        //拼接key
+        // key是在商户平台API安全里自己设置的
+        $stringSignTemp = MD5($stringSignTemp);       //将字符串进行MD5加密
+        $sign = strtoupper($stringSignTemp);      //将所有字符转换为大写
+        return $sign;
+    }
+
+
+    public function wxpay()
+    {
+        $order_no = input('post.order_no');
+        $tab = Db::table('store_main_orders')->where(['order_no' => $order_no, 'order_status' => 2])->find();
+
+        if ($tab) {
+            $WxPayConfig = new \WxPayConfig();
+            //调用随机字符串生成方法获取随机字符串
+            $nonce_str = $this->rand_code();
+            //appid
+            $data['appid'] = $WxPayConfig::APPID;
+            //商户号
+            $data['mch_id'] = $WxPayConfig::MCHID;
+            //商品描述
+            $data['body'] = '保洁项目';
+            //ip地址
+//            $data['spbill_create_ip'] = '116.255.206.241';
+            //金额
+//            $total_fee = $tab['amount'];
+            $total_fee = 0.01;
+            $data['total_fee'] = $total_fee * 100;
+            //商户订单号,不能重复
+            $data['out_trade_no'] = $order_no;
+            //随机字符串
+            $data['nonce_str'] = $nonce_str;
+            //        $data['notify_url'] = 'https://yp.dxshuju.com/user/Pay/receiveNotify';   //回调地址,用户接收支付后的通知,必须为能直接访问的网址,不能跟参数
+            //回调地址,用户接收支付后的通知,必须为能直接访问的网址,不能跟参数
+            $data['notify_url'] = $WxPayConfig::NOTIFY_URL;
+            //支付方式
+            $data['trade_type'] = 'APP';
+            //将参与签名的数据保存到数组  注意：以上几个参数是追加到$data中的，$data中应该同时包含开发文档中要求必填的剔除sign以外的所有数据
+            //获取签名
+            $data['sign'] = $this->getSign($data);
+            $xml = $this->ToXml($data);
+            //数组转xml
+            //curl 传递给微信方
+            $url = "https://api.mch.weixin.qq.com/pay/unifiedorder";
+
+            $ch = curl_init();
+            curl_setopt($ch, CURLOPT_URL, $url);
+            if (stripos($url, "https://") !== FALSE) {
+                curl_setopt($ch, CURLOPT_SSLVERSION, 1);
+                curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+                curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
+            } else {
+                curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, TRUE);
+                //严格校验
+                curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
+            }
+
+            //设置header
+            curl_setopt($ch, CURLOPT_SSLVERSION, 1);
+            curl_setopt($ch, CURLOPT_HEADER, FALSE);
+            //要求结果为字符串且输出到屏幕上
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+            //设置超时
+            curl_setopt($ch, CURLOPT_TIMEOUT, 30);
+            curl_setopt($ch, CURLOPT_POST, TRUE);
+            //传输文件
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $xml);
+            //运行curl
+            $data = curl_exec($ch);
+            //返回结果
+
+
+            if ($data) {
+                curl_close($ch);
+                //返回成功,将xml数据转换为数组.
+                $re = $this->FromXml($data);
+                //修改表中的预支付id
+                Db::table('store_main_orders')->where(['order_no' => $order_no, 'order_status' => 2])->update(['prepay_id' => $re['prepay_id']]);
+
+                if ($re['return_code'] != 'SUCCESS') {
+                    return json([
+                        'status' => 0,
+                        'message' => '微信支付统一下单失败'
+                    ]);
+                } else {
+                    //接收微信返回的数据,传给APP!
+                    $arr = array(
+                        //预支付交易会话标识
+                        'prepayid' => $re['prepay_id'],
+                        //应用APPID
+                        'appid' => $WxPayConfig::APPID,
+                        //商户号
+                        'partnerid' => $WxPayConfig::MCHID,
+                        //随机字符串
+                        'noncestr' => $nonce_str,
+                        //时间
+                        'timestamp' => time(),
+                        //扩展字段
+                        'package' => 'Sign=WXPay'
+                    );
+                    //第二次生成签名
+                    $sign = $this->getSign($arr);
+                    $arr['sign'] = $sign;
+                    return json([
+                        'status' => 1,
+                        'message' => '微信支付统一下单成功',
+                        'result' => $arr
+                    ]);
+                }
+            } else {
+                $error = curl_errno($ch);
+                curl_close($ch);
+                return json([
+                    'status' => 0,
+                    'message' => '微信支付统一下单失败'
+                ]);
+            }
+        } else {
+            return json([
+                'status' => 0,
+                'message' => '微信支付统一下单失败'
+            ]);
+        }
+    }
+
+    public function FromXml($xml)
+    {
+        if (!$xml) {
+            echo "xml数据异常！";
+        }
+        //将XML转为array
+        //禁止引用外部xml实体
+        libxml_disable_entity_loader(true);
+        $data = json_decode(json_encode(simplexml_load_string($xml, 'SimpleXMLElement', LIBXML_NOCDATA)), true);
+        return $data;
+    }
+
+    public function Notify()
+    {
+        //接收微信返回的数据数据,返回的xml格式
+        $xmlData = file_get_contents('php://input');
+        //将xml格式转换为数组
+        $data = $this->FromXml($xmlData);
+        //用日志记录检查数据是否接受成功，验证成功一次之后，可删除。
+        $file = fopen('./log.txt', 'a+');
+        fwrite($file, var_export($data, true));
+        //为了防止假数据，验证签名是否和返回的一样。
+        //记录一下，返回回来的签名，生成签名的时候，必须剔除sign字段。
+        $sign = $data['sign'];
+        unset($data['sign']);
+        if ($sign == $this->getSign($data)) {
+            //签名验证成功后，判断返回微信返回的
+            if ($data['result_code'] == 'SUCCESS') {
+                //根据返回的订单号做业务逻辑
+                $arr = array(
+                    'order_status' => 3,
+                    'transaction_id' => $data['transaction_id'],
+                    'pay_time' => date('Y-m-d H:i:s', time()),
+                    'update_time' => date('Y-m-d H:i:s', time())
+                );
+
+                $re = Db::table('store_main_orders')->where(['order_no' => $data['out_trade_no']])->update($arr);
+
+                //处理完成之后，告诉微信成功结果！
+                if ($re) {
+                    echo '<xml>
+              <return_code><![CDATA[SUCCESS]]></return_code>
+              <return_msg><![CDATA[OK]]></return_msg>
+              </xml>';
+                    exit();
+                }
+            } //支付失败，输出错误信息
+            else {
+                $file = fopen('./log.txt', 'a+');
+                fwrite($file, "错误信息：" . $data['return_msg'] . date("Y-m-d H:i:s"), time() . "\r\n");
+            }
+        } else {
+            $file = fopen('./log.txt', 'a+');
+            fwrite($file, "错误信息：签名验证失败" . date("Y-m-d H:i:s"), time() . "\r\n");
+        }
+
+    }
+
+
+}
--- application/index/controller/Order.php
+++ application/index/controller/Order.php
@@ -8,8 +8,6 @@ class Order extends Controller{
 
     public function userInfo()
     {
-
-
         $token = input('post.token');
         $telephone = input('post.telephone');
         $where = [
@@ -28,7 +26,7 @@ class Order extends Controller{
 
     }
 
-    //获取订单信息(普通商城版) 31
+    //获取订单信息(普通商城版)
     public function getOrderInfo(){
         $this->userInfo();
         //获取参数
@@ -36,23 +34,11 @@ class Order extends Controller{
         $res = model('Order')->getOrderInfo($param);
         echo json_encode(array(
             'statuscode'  => 1,
-            'message'   =>  '获取订单信息(普通商城版)成功',
             'result'      => $res
         ));
         exit;
     }
 
-    //获取订单信息(多用户版)
-    public function getOrderInfoMulti(){
-        //获取参数
-        $param = input('post.');
-        $res = model('Order')->getOrderInfoMulti($param);
-        echo json_encode(array(
-            'statuscode'  => 1,
-            'result'      => $res
-        ));
-        exit;
-    }
 
     //获取订单详情信息(普通商城版)
     public function getOrderDetailInfo(){
@@ -62,49 +48,13 @@ class Order extends Controller{
         $res = model('Order')->getOrderDetailInfo($param);
         echo json_encode(array(
             'statuscode'  => 1,
-            'message'   =>  '获取订单详情信息(普通商城版)成功',
+            'message'   =>  '获取订单详情信息成功',
             'result'      => $res
         ));
         exit;
     }
 
-    //获取订单详情信息(拼团版)
-    public function getGroupOrderDetailInfo(){
-        //获取参数
-        $param = input('post.');
-        $res = model('Order')->getGroupOrderDetailInfo($param);
-        echo json_encode(array(
-            'statuscode'  => 1,
-            'result'      => $res
-        ));
-        exit;
-    }
-
-    //获取订单信息(拼团单独购买版)
-    public function getOrderInfoBySingle(){
-        //获取参数
-        $param = input('post.');
-        $res = model('Order')->getOrderInfoBySingle($param);
-        echo json_encode(array(
-            'statuscode'  => 1,
-            'result'      => $res
-        ));
-        exit;
-    }
-
-    //获取订单信息(多用户拼团版)
-    public function getOrderInfoBySingleMulti(){
-        //获取参数
-        $param = input('post.');
-        $res = model('Order')->getOrderInfoBySingleMulti($param);
-        echo json_encode(array(
-            'statuscode'  => 1,
-            'result'      => $res
-        ));
-        exit;
-    }
-
-    //生成订单(普通商城版)       440
+    //生成订单(普通商城版)
     public function makeOrder(){
         $this->userInfo();
         //获取参数
@@ -125,42 +75,6 @@ class Order extends Controller{
             ));
             exit;
         }
-    }
-
-    //积分商品生成订单(多用户版)
-    public function makeJfOrder(){
-        //获取参数
-        $param = input('post.');
-        $res = model('Order')->makeJfOrder($param);
-        echo json_encode(array(
-            'statuscode'  => 1,
-            'result'      => $res
-        ));
-        exit;
-    }
-
-    //生成订单(拼团单独购买版)
-    public function makeOrderBySingle(){
-        //获取参数
-        $param = input('post.');
-        $res = model('Order')->makeOrderBySingle($param);
-        echo json_encode(array(
-            'statuscode'  => 1,
-            'result'      => $res
-        ));
-        exit;
-    }
-
-    //生成订单(商城多用户版)
-    public function makeOrderMulti(){
-        //获取参数
-        $param = input('post.');
-        $res = model('Order')->makeOrderMulti($param);
-        echo json_encode(array(
-            'statuscode'  => 1,
-            'result'      => $res
-        ));
-        exit;
     }
 
     /*
@@ -198,31 +112,6 @@ class Order extends Controller{
         }
     }
 
-    //更改订单状态为已付款(拼团单独购买版)
-    public function updateOrderStatusBySingle(){
-        $order_id = input('post.order_id');
-        $from = input('post.from');
-        if($from == 'single'){
-            $data['order_status'] = 3;
-        }else{
-            $data['order_status'] = 2;
-        }
-
-        $data['update_time'] = date('Y-m-d H:i:s');
-        $data['pay_time'] = date('Y-m-d H:i:s');
-        Db::table('store_group_main_orders')->where('id = '.$order_id)->update($data);
-    }
-
-    //更改积分订单状态
-    public function updateJfOrderStatus(){
-        $order_id = input('post.order_id');
-        $data['order_status'] = 3;
-
-        $data['update_time'] = date('Y-m-d H:i:s');
-        $data['pay_time'] = date('Y-m-d H:i:s');
-        Db::table('store_main_orders')->where('id = '.$order_id)->update($data);
-    }
-
     /*
      *根据id查询订单号和总金额(普通商城版)
      */
@@ -234,6 +123,7 @@ class Order extends Controller{
     //关联表           store_main_orders   订单表
 
     public function getOrderInfoByOrderId(){
+        $this->userInfo();
         $order_id = input('post.order_id');
         $res = Db::table('store_main_orders')
             ->field('order_no,total_amount')
@@ -256,18 +146,6 @@ class Order extends Controller{
         }
     }
 
-
-    //根据id查询订单号和总金额(拼团单独购买版)
-    public function getOrderInfoByOrderIdBySingle(){
-        $order_id = input('post.order_id');
-        $res = Db::table('store_group_main_orders')->field('order_no,total_amount')->where('id = '.$order_id)->find();
-        echo json_encode(array(
-            'statuscode'  => 1,
-            'result'      => $res
-        ));
-        exit;
-    }
-
     /*
      *确认订单(普通商城版)
      */
@@ -280,6 +158,7 @@ class Order extends Controller{
 
 
     public function confirmOrder(){
+        $this->userInfo();
         $order_id = input('post.order_id');
         $data['order_status'] = 5;
         $res = Db::table('store_main_orders')->where('id = '.$order_id)->update($data);
@@ -287,29 +166,17 @@ class Order extends Controller{
         {
             echo json_encode(array(
                 'statuscode'  => 1,
-                'message'      => '确认订单(普通商城版)成功',
+                'message'      => '确认订单成功',
                 'result'      => $res
             ));
             exit;
         }else{
             echo json_encode(array(
                 'statuscode'  => 0,
-                'message'      => '确认订单(普通商城版)失败'
+                'message'      => '确认订单失败'
             ));
             exit;
         }
-    }
-
-    //确认订单(拼团版)
-    public function confirmGroupOrder(){
-        $order_id = input('post.order_id');
-        $data['order_status'] = 5;
-        $res = Db::table('store_group_main_orders')->where('id = '.$order_id)->update($data);
-        echo json_encode(array(
-            'statuscode'  => 1,
-            'result'      => $res
-        ));
-        exit;
     }
 
     /*
@@ -323,36 +190,25 @@ class Order extends Controller{
     //关联表           store_main_orders   订单表
 
     public function cancelOrder(){
-        $order_id = input('post.order_id');
-        $data['status'] = -1;
-        $res = Db::table('store_main_orders')->where('id = '.$order_id)->update($data);
+        $this->userInfo();
+        $order_no = input('post.order_no');
+        $data['order_status'] = -1;
+        $res = Db::table('store_main_orders')->where("order_no = '$order_no'")->update($data);
         if ($res)
         {
             echo json_encode(array(
                 'statuscode'  => 1,
-                'message'      => '取消订单(普通商城版)成功',
+                'message'      => '取消订单成功',
                 'result'      => $res
             ));
             exit;
         }else{
             echo json_encode(array(
                 'statuscode'  => 0,
-                'message'      => '取消订单(普通商城版)失败'
+                'message'      => '取消订单失败'
             ));
             exit;
         }
-    }
-
-    //取消订单(拼团单独购买版)
-    public function cancelOrderBySingle(){
-        $order_id = input('post.order_id');
-        $data['status'] = -1;
-        $res = Db::table('store_group_main_orders')->where('id = '.$order_id)->update($data);
-        echo json_encode(array(
-            'statuscode'  => 1,
-            'result'      => $res
-        ));
-        exit;
     }
 
     //设置主订单表推荐人及佣金信息(普通商城版)
@@ -360,18 +216,6 @@ class Order extends Controller{
         $order_id = input('post.order_id');
         $rec_id = input('post.rec_id');
         $res = model('Order')->setMainRecInfo($order_id,$rec_id);
-        echo json_encode(array(
-            'statuscode'  => 1,
-            'result'      => $res
-        ));
-        exit;
-    }
-
-    //设置主订单表推荐人及佣金信息(拼团单独购买版)
-    public function setMainRecInfoBySingle(){
-        $order_id = input('post.order_id');
-        $rec_id = input('post.rec_id');
-        $res = model('Order')->setMainRecInfoBySingle($order_id,$rec_id);
         echo json_encode(array(
             'statuscode'  => 1,
             'result'      => $res
@@ -477,150 +321,6 @@ class Order extends Controller{
     function encrypt($data, $appkey) {
         return urlencode(base64_encode(md5($data.$appkey)));
     }
-
-    //参团模式更新订单信息
-    public function updateOrderByJoinGroup(){
-        //获取参数
-        $group_num = input('post.group_num');
-        //获取数据
-        $where = "group_num = '$group_num' and group_identity = 1";
-        $res = Db::table('store_group_main_orders')->field('pintuan_count')->where($where)->find();
-        $pintuan_count = $res['pintuan_count'];
-        $where1 = "group_num = '$group_num' and (order_status = 2 or order_status = 3)";
-        $count = Db::table('store_group_main_orders')->where($where1)->count();
-        if($pintuan_count <= $count){
-            $data = [
-                'group_status'  => 2,
-                'order_status'  => 3,
-            ];
-            //更新订单表
-            Db::table('store_group_main_orders')->where($where1)->update($data);
-            echo json_encode(array(
-                'statuscode'  => 1
-            ));
-            exit;
-        }else{
-            echo json_encode(array(
-                'statuscode'  => 0
-            ));
-            exit;
-        }
-    }
-
-    //检验当前是否已成团(通过团号)
-    public function checkGroupStatusByGroupNum(){
-        //获取参数
-        $group_num = input('post.group_num');
-        //获取数据
-        $where = "group_num = '$group_num' and group_identity = 1";
-        $res = Db::table('store_group_main_orders')->field('group_status')->where($where)->find();
-        if($res['group_status'] != 2){
-            echo json_encode(array(
-                'statuscode'  => 0
-            ));
-            exit;
-        }else{
-            echo json_encode(array(
-                'statuscode'  => 1
-            ));
-            exit;
-        }
-    }
-
-    //拼团-分享时获取订单信息
-    public function getOrderInfoByShare(){
-        //获取参数
-        $order_id = input('post.order_id');
-        $res = Db::table('store_group_main_orders')->alias('gmo')->field('gmo.id as order_id,gmo.group_num,gmo.pintuan_count,pro.p_name,pro.associator_price,pro.pintuan_price,img.thumb')
-            ->join('store_products pro','gmo.pro_id = pro.id','LEFT')
-            ->join('store_pro_images img','pro.id = img.p_id','LEFT')
-            ->where('gmo.id = '.$order_id)
-            ->find();
-
-        $group_num = $res['group_num'];
-        //获取购买人信息
-        $where = "gmo.group_num = '$group_num' and (gmo.order_status = 2 or gmo.order_status = 3)";
-        $mem_res = Db::table('store_group_main_orders')->alias('gmo')->field('gmo.group_identity,mem.avatarUrl')
-            ->join('store_members mem','gmo.mem_id = mem.mem_id','LEFT')
-            ->where($where)
-            ->order('gmo.create_time asc')
-            ->select();
-
-        //获取团长mem_id
-        $where1 = "group_num = '$group_num' and group_identity = 1 and (order_status = 2 or order_status = 3)";
-        $tuanzhang_res = Db::table('store_group_main_orders')->field('mem_id')
-            ->where($where1)
-            ->find();
-        $mem_id = $tuanzhang_res['mem_id'];
-
-        $pintuan_count = $res['pintuan_count'];
-        $count = count($mem_res);
-        if($count < $pintuan_count){
-            $lack_count = $pintuan_count - $count;
-            $temp_array = [
-                'group_identity'  => 2,
-                'avatarUrl'  => '/pics/icons/wenhao.png'
-            ];
-            for($i = 0;$i < $lack_count;$i++){
-                array_push($mem_res,$temp_array);
-            }
-            $is_enough = false;
-        }else{
-            $is_enough = true;
-        }
-
-        echo json_encode(array(
-            'statuscode'  => 1,
-            'order_res'   => $res,
-            'mem_res'     => $mem_res,
-            'is_enough'   => $is_enough,
-            'mem_id'      => $mem_id
-        ));
-        exit;
-    }
-
-    //拼团-分享时获取商品id
-    public function getProIdByShare(){
-        //获取参数
-        $order_id = input('post.order_id');
-        $res = Db::table('store_group_main_orders')->field('pro_id,group_num')
-            ->where('id = '.$order_id)
-            ->find();
-        $pro_id = $res['pro_id'];
-        $group_num = $res['group_num'];
-
-        echo json_encode(array(
-            'statuscode'  => 1,
-            'pro_id'   => $pro_id,
-            'group_num'   => $group_num
-        ));
-        exit;
-    }
-
-    //检验当前是否已成团
-    public function checkGroupStatusByOrderId(){
-        //获取参数
-        $order_id = input('post.order_id');
-        //获取团号
-        $group_res = Db::table('store_group_main_orders')->field('group_num')->where('id = '.$order_id)->find();
-        $group_num = $group_res['group_num'];
-        //获取数据
-        $where = "group_num = '$group_num' and group_identity = 1";
-        $res = Db::table('store_group_main_orders')->field('group_status')->where($where)->find();
-        if($res['group_status'] != 2){
-            echo json_encode(array(
-                'statuscode'  => 0
-            ));
-            exit;
-        }else{
-            echo json_encode(array(
-                'statuscode'  => 1
-            ));
-            exit;
-        }
-    }
-
-    //支付成功后
 
 }
 
